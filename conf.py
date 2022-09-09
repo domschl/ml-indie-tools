11d8ad0a61d0849efc794e865871933120f90a1a
Dominik Schlösser <dsc@dosc.net>
2022-09-09 09:31:26 +0200

    0.3.8 prep

--- conf.py
+++ conf.py
@@ -23,7 +23,7 @@ author = 'dsc'
 
 # The full version, including alpha/beta/rc tags
 # sync with setup.py
-release = '0.1.4'
+release = '0.3.8'
 
 
 # -- General configuration ---------------------------------------------------
