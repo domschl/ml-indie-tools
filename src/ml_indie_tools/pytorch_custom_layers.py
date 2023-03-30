0453bd863462d04226d8ed47a1466da3e8a3aa47
Dominik Schlösser <dsc@dosc.net>
2023-03-30 22:53:51 +0200

    Bad recursive replacement for final lin-layer, more8

--- src/ml_indie_tools/pytorch_custom_layers.py
+++ src/ml_indie_tools/pytorch_custom_layers.py
@@ -131,8 +131,12 @@ class FeedFoward(nn.Module):
                 self.net2 = nn.LSTM(hidden_size, hidden_size, num_layers=2)
                 self.net3 = nn.Linear(hidden_size, input_size)
                 self.lstm_lyr = 2
-                self.c = torch.randn(self.lstm_lyr, 128, hidden_size, device="mps")
-                self.h = torch.randn(self.lstm_lyr, 128, hidden_size, device="mps")
+                self.c = torch.randn(
+                    self.lstm_lyr, 128, hidden_size, device="mps"
+                ).requires_grad(False)
+                self.h = torch.randn(
+                    self.lstm_lyr, 128, hidden_size, device="mps"
+                ).requires_grad(False)
                 self.rec = True
             else:
                 self.net = nn.Sequential(
