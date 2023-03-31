57cfe145bf08ee574c35d8e1fa8f60dcaf63335e
Dominik Schlösser <dsc@dosc.net>
2023-03-31 10:48:27 +0200

    Eliminate unnecessary imports

--- src/ml_indie_tools/pytorch_tr_compr_layers.py
+++ src/ml_indie_tools/pytorch_tr_compr_layers.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
-from pytorch_custom_layers import SelfAttentionHead, MultiHeadAttention
+from pytorch_custom_layers import MultiHeadAttention
 
 #
 # Many parts are taken from https://github.com/karpathy/ng-video-lecture/blob/master/gpt.py
