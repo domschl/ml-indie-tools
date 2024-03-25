fa9731f96b03f0119127afbc1ea1e211181d9d0b
Dominik Schlösser <dominik.schloesser@gmail.com>
2024-03-25 14:41:01 +0100

    Doc updates

--- conf.py
+++ conf.py
@@ -12,18 +12,19 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('src/ml_indie_tools'))
+
+sys.path.insert(0, os.path.abspath("src/ml_indie_tools"))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'ml-indie-tools'
-copyright = '2022, dsc'
-author = 'dsc'
+project = "ml-indie-tools"
+copyright = "2022-2024, dsc"
+author = "dsc"
 
 # The full version, including alpha/beta/rc tags
 # sync with setup.py
-release = '0.3.8'
+release = "0.11.0"
 
 
 # -- General configuration ---------------------------------------------------
@@ -32,20 +33,20 @@ release = '0.3.8'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
-    'sphinx.ext.mathjax',
-    'm2r2',   # pip install m2r2  # needed for markdown to rst conversion
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.githubpages",
+    "sphinx.ext.mathjax",
+    # "m2r2",  # pip install m2r2  # needed for markdown to rst conversion
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -53,9 +54,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'bizstyle'
+html_theme = "bizstyle"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
